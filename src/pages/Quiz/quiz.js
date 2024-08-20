b50862891a00ba997f2abfe873fe5a9ffb6ae1f3
Julia Ayumi <julia.uno@fatec.sp.gov.br>
2024-08-20 11:16:09 -0300

    feat: frontend

---
+++ src/pages/Quiz/quiz.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const blocosPergunta = document.querySelectorAll('.area-pergunta');
+
+    blocosPergunta.forEach((bloco) => {
+        const respostas = bloco.querySelectorAll('.resposta-item');
+
+        respostas.forEach((resposta) => {
+            resposta.addEventListener('click', () => {
+                const isSelected = resposta.checked;
+
+                respostas.forEach((element) => {
+                    if (element !== resposta) {
+                        element.checked = false;
+                    }
+                });
+            });
+        });
+    });
+});
+
+function verificarResposta() {
+    const blocosPergunta = document.querySelectorAll('.area-pergunta');
+    let todasAsRespostasSelecionadas = true;
+
+    blocosPergunta.forEach((bloco) => {
+        const respostasSelecionadas = bloco.querySelectorAll('.resposta-item:checked');
+        if (respostasSelecionadas.length === 0) {
+            todasAsRespostasSelecionadas = false;
+        }
+    });
+
+    if (todasAsRespostasSelecionadas) {
+        alert('Finalizado.');
+    } else {
+        alert('Por favor, selecione uma resposta em cada bloco antes de avançar.');
+    }
+}
+
+function verificarSaida() {
+    if (confirm("Se você sair, perderá todo o progresso. Deseja continuar?")) {
+        window.location.href = "../Quizes/quizes.html";
+    }
+}
